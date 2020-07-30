a83c47c5fe22b8f35feecd4f1e0b2758e06f943a
Robert B <robertb@lifesciencepublishers.com>
2020-07-30 09:11:51 -0600

    add JavaScript api

---
+++ script.js
@@ -0,0 +1,17 @@
+// Get quote from API
+
+async function getQuote(){
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response = await fetch(proxyUrl + apiURL)
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        getQuote();
+        console.log('whoops, no quote', error)
+    }
+
+}
+
+getQuote()
