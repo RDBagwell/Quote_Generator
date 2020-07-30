d18e7bc734ac8f3d17c5d4fc6e82417b1a905909
Robert B <robertb@lifesciencepublishers.com>
2020-07-30 14:03:27 -0600

    functionallity is added

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
 // Get quote from API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+let tries = 0;
 
-async function getQuote(){
+async function getQuote(check = 0){
+    showSpinner();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
         const response = await fetch(proxyUrl + apiURL)
         const data = await response.json();
-        console.log(data);
+        
+        if(data.quoteAuthor === ''){
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+
+        if(data.quoteText.length > 120){
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        removeSpinner();
     } catch (error) {
-        getQuote();
-        console.log('whoops, no quote', error)
+        if(tries > 5){
+            removeSpinner();
+            console.log('whoops, no quote', error)
+            tries = 0
+        } else {
+            tries++;
+            getQuote();
+        }
     }
 
 }
 
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+    window.open(twitterUrl, '_blank');
+}
+
+function showSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeSpinner(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 getQuote()
